2dd2bc1d0ac48d19320ee7846cfb1e8ffdb9331b
naathaliie <nathalielarsson@live.se>
2024-04-17 08:56:43 +0200

    stuff

--- src/components/ShowFromAPI/ShowFromAPIBOX.tsx
+++ src/components/ShowFromAPI/ShowFromAPIBOX.tsx
@@ -1,25 +1,25 @@
 /* Hämta info från API som skall skickas til card som i sin tur skall
 rendera på sidan med hjälp av useEffect*/
-import { useState } from "react";
 import "./ShowFromAPIBOX.scss";
 import ShowFromAPICARD from "./ShowFromAPICARD";
 import { useFetch } from "../../hooks/useFetch";
 
+//APIet
 const API = "https://swapi.py4e.com/api/people/";
+
 const ShowFromAPIBOX = () => {
-  //Kallar på min custom-hook useFetch som tar emot en property (API) och vi destructurerar
-  //ut theData som innehåller det vi får tillbaka från APIet
+  //Destructurerar ut theData i från min custom-hook useFetch
   const { theData } = useFetch(API);
 
-  //consolLoggar theData NÄR den har fått tillbaka från useFetch
+  //consolLoggar theData --->NÄR<--- den har fått tillbaka från useFetch
   theData.length > 0 && console.log("hejsvejs, ", theData[3]);
 
   return (
     <div className="ShowFromAPIBOX">
       <h3>Hämta info från ett API</h3>
 
-      {/* NÄR datan har hämtats från useFetch så kallar vi på showFromAPICard där vi skickar med ett specifikt indec i vår theData */}
-      {theData.length > 0 ? <ShowFromAPICARD oneSw={theData[0]} /> : "laddar"}
+      {/* --->NÄR<--- datan har hämtats från useFetch så kallar vi på showFromAPICard där vi skickar med ett specifikt index i vår theData */}
+      {theData.length > 0 ? <ShowFromAPICARD oneSw={theData[4]} /> : "laddar"}
     </div>
   );
 };
